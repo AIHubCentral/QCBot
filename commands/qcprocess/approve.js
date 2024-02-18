273556b3c53d84422f68978e7d7affd8248ce5ef
Antasma245 <144015963+Antasma245@users.noreply.github.com>
2024-02-18 20:54:40 +0100

    Added error handling for '/approve' and '/reject' In case the bot isn't able to find the original submission message (e.g. if it has been deleted), it'll now send the approval/rejection message without replying to it

--- commands/qcprocess/approve.js
+++ commands/qcprocess/approve.js
@@ -46,6 +46,8 @@ module.exports = {
         const userId = tag.get('dbUserId');
         const submissionLink = tag.get('dbSubmissionLink');
 
+        const qcName = interaction.member.nickname ?? interaction.member.displayName;
+
         const member = await interaction.guild.members.fetch(userId);
         const modelRole = await interaction.guild.roles.fetch(modelRoleId);
 
@@ -55,8 +57,6 @@ module.exports = {
             await SubmissionsTable.destroy({ where: { dbSubmissionId: inputSubmissionId } });
     
             const tag2 = await StatsTable.findOne({ where: { dbQcId: interaction.user.id } });
-        
-            const qcName = interaction.member.nickname ?? interaction.member.displayName;
     
             if (!tag2) {
                 await StatsTable.create({
@@ -77,14 +77,25 @@ module.exports = {
 
             await interaction.editReply(`Submission successfully approved.\nYou may now dismiss this message.`);
 		} catch (error) {
-            await interaction.editReply(`Something went wrong... Please try again and see if it works. If the error still persists, consider pinging <@${myUserId}>.`);            
-            console.error(`New error report! Occured on ${new Date().toUTCString()} while executing '/${interaction.commandName}'`);
-            console.log(error);
+            if (error.message.includes('Unknown message')) {
+                await interaction.channel.send(`<@${userId}> Your submission (ID: ${inputSubmissionId}) has been approved by **${qcName}**.\nComment: *${comment}*\nYou've been granted the Model Maker role and can now post in the https://discord.com/channels/${guildId}/${modelsChannelId} channel.`);
+
+                await interaction.editReply(`Submission successfully approved.\nYou may now dismiss this message.`);
+            } else {
+                await interaction.editReply(`Something went wrong... Please try again and see if it works. If the error still persists, consider pinging <@${myUserId}>.`);            
+                console.error(`New error report! Occured on ${new Date().toUTCString()} while executing '/${interaction.commandName}'`);
+                console.log(error);
+            }
 		}
 
         if (approvalLogsId) {
-            const approvalLogsThread = await interaction.guild.channels.fetch(approvalLogsId);
-            await approvalLogsThread.send({ content: `<@460577350900514837>`, embeds: [new EmbedBuilder().setColor(`e74c3c`).setTitle('New voice model approved').setDescription(`**ID:** ${inputSubmissionId}\n**Submitted by:** <@${userId}>\n**Link:** ${submissionLink}\n\n**Approved by:** <@${interaction.user.id}>\n**Comment:** *${comment}*`)] });
+            try {
+                const approvalLogsThread = await interaction.guild.channels.fetch(approvalLogsId);
+                await approvalLogsThread.send({ content: `<@460577350900514837>`, embeds: [new EmbedBuilder().setColor(`e74c3c`).setTitle('New voice model approved').setDescription(`**ID:** ${inputSubmissionId}\n**Submitted by:** <@${userId}>\n**Link:** ${submissionLink}\n\n**Approved by:** <@${interaction.user.id}>\n**Comment:** *${comment}*`)] });
+            } catch (error) {
+                console.error(`New error report! Occured on ${new Date().toUTCString()} while executing '/${interaction.commandName}'`);
+                console.log(error);
+            }
         }
     }
 };
